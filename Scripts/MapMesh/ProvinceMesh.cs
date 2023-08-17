e467c27820f811904c97880a1de02a9bd01723a4
Nashet <not@mmm.com>
2023-08-18 00:54:55 +0200

    Initial

---
+++ Scripts/MapMesh/ProvinceMesh.cs
@@ -0,0 +1,160 @@
+﻿using Nashet.Map.Utils;
+using Nashet.MeshData;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nashet.MapMeshes
+{
+	public class ProvinceMesh : IProvinceMesh
+	{
+		private static readonly Dictionary<int, ProvinceMesh> lookUp = new Dictionary<int, ProvinceMesh>();
+
+		public int ID { get; protected set; }
+
+		public GameObject GameObject { get; protected set; }
+		protected MeshFilter MeshFilter { get; private set; }
+
+		protected MeshRenderer meshRenderer;
+
+		public Vector3 Position { get; protected set; }
+		private readonly Dictionary<int, MeshRenderer> bordersMeshes = new Dictionary<int, MeshRenderer>();
+		private Material defaultMaterial;
+
+
+		public ProvinceMesh(int ID, MeshStructure meshStructure, Dictionary<int, MeshStructure> neighborBorders,
+			Color provinceColor, Transform parent, Material defaultBorderMaterial, string name = "", Material material = null)
+		{
+			this.ID = ID;
+			defaultMaterial = material;
+
+			//spawn object
+			GameObject = new GameObject(string.Format("{0} {1}", ID, name));
+
+			//Add Components
+			MeshFilter = GameObject.AddComponent<MeshFilter>();
+			meshRenderer = GameObject.AddComponent<MeshRenderer>();
+
+			// in case you want the new gameobject to be a child
+			// of the gameobject that your script is attached to
+			GameObject.transform.parent = parent;
+
+			var landMesh = MeshFilter.mesh;
+
+			landMesh.vertices = meshStructure.getVertices().ToArray();
+			landMesh.triangles = meshStructure.getTriangles().ToArray();
+			SetUV(landMesh); // dont use getUVmap()
+
+			landMesh.RecalculateNormals();
+			landMesh.RecalculateBounds();
+			landMesh.name = ID.ToString();
+			landMesh.Optimize();
+			landMesh.UploadMeshData(true);
+
+			Position = setProvinceCenter(meshStructure);// I can use mesh.bounds.center, but it will center off a little bit
+
+
+			MeshCollider groundMeshCollider = GameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
+			groundMeshCollider.sharedMesh = MeshFilter.mesh;
+
+			meshRenderer.material = material;
+
+			meshRenderer.material.color = provinceColor;
+
+
+			//making meshes for border
+			foreach (var border in neighborBorders)
+			{
+				//each color is one neighbor (non repeating)
+				//World.ProvincesById.Keys.(border.Key, out var neighbor);
+				var neighbor = border.Key;
+				//if (neighbor != null)
+				{
+					//if (!IsForDeletion)
+					{
+						GameObject borderObject = new GameObject($"Border with {neighbor}");
+
+						//Add Components
+						MeshFilter = borderObject.AddComponent<MeshFilter>();
+						MeshRenderer meshRenderer = borderObject.AddComponent<MeshRenderer>();
+
+						borderObject.transform.parent = GameObject.transform;
+
+						Mesh borderMesh = MeshFilter.mesh;
+						borderMesh.vertices = border.Value.getVertices().ToArray();
+						borderMesh.triangles = border.Value.getTriangles().ToArray();
+						borderMesh.uv = border.Value.getUVmap().ToArray();
+						borderMesh.RecalculateNormals();
+						borderMesh.RecalculateBounds();
+						meshRenderer.material = defaultBorderMaterial;
+						borderMesh.Optimize();
+						borderMesh.UploadMeshData(true);
+
+						bordersMeshes.Add(neighbor, meshRenderer);
+					}
+				}
+			}
+			lookUp.Add(ID, this);
+		}
+
+		private static void SetUV(Mesh landMesh)
+		{
+			Vector2[] uvCoordinates = new Vector2[landMesh.vertices.Length];
+
+			for (int i = 0; i < landMesh.vertices.Length; i++)
+			{
+				uvCoordinates[i] = new Vector2(landMesh.vertices[i].x, landMesh.vertices[i].y);
+			}
+
+			landMesh.uv = uvCoordinates;
+		}
+
+		public void SetColor(Color color)
+		{
+			if (meshRenderer.material != defaultMaterial)
+				meshRenderer.material = defaultMaterial;
+			meshRenderer.material.color = color;
+		}
+
+		public void SetMaterial(Material material) => meshRenderer.material = material;
+
+		private Vector3 setProvinceCenter(MeshStructure meshStructure)
+		{
+			Vector3 accu = new Vector3(0, 0, 0);
+			foreach (var c in meshStructure.getVertices())
+				accu += c;
+			accu = accu / meshStructure.verticesCount;
+			return accu;
+		}
+
+		public static ProvinceMesh GetById(int id)
+		{
+			return lookUp[id];
+		}
+
+		public static int? GetIdByCollider(Collider collider)
+		{
+			if (collider != null)
+			{
+				MeshCollider meshCollider = collider as MeshCollider;
+				if (meshCollider == null || meshCollider.sharedMesh == null)
+					return null;
+
+				Mesh mesh = meshCollider.sharedMesh;
+
+				if (mesh.name == "Quad")
+					return null;
+
+				int provinceNumber = Convert.ToInt32(mesh.name);
+				return provinceNumber;
+			}
+			else
+				return null;
+		}
+
+		public void SetBorderMaterial(int id, Material material)
+		{
+			bordersMeshes[id].material = material;
+		}
+	}
+}
---
+++ Shaders/Stripes.shader
@@ -0,0 +1,50 @@
+﻿Shader "Custom/StripedShader"
+{
+    Properties
+    {
+        _MainTex ("Main Texture", 2D) = "white" {}
+        _StripeColor1 ("Stripe Color 1", Color) = (1, 1, 1, 1)
+        _StripeColor2 ("Stripe Color 2", Color) = (0, 0, 0, 1)
+        _StripeWidth ("Stripe Width", Range(0.0001, 10)) = 0.01
+        _StripeAngle ("Stripe Angle", Range(-180, 180)) = 45
+    }
+ 
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+        
+        CGPROGRAM
+        #pragma surface surf Lambert
+        
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+        
+        sampler2D _MainTex;
+        fixed4 _StripeColor1;
+        fixed4 _StripeColor2;
+        float _StripeWidth;
+        float _StripeAngle;
+ 
+        void surf (Input IN, inout SurfaceOutput o)
+        {
+            // Rotate the UV coordinates based on the stripe angle
+            float2 rotatedUV = IN.uv_MainTex;
+            rotatedUV = float2(rotatedUV.x * cos(_StripeAngle) - rotatedUV.y * sin(_StripeAngle),
+                               rotatedUV.x * sin(_StripeAngle) + rotatedUV.y * cos(_StripeAngle));
+            
+            // Calculate the position along the inclined stripes
+            float stripePos = frac(rotatedUV.x / _StripeWidth);
+            
+            // Alternate between stripe colors
+            fixed4 stripeColor = (stripePos < 0.5) ? _StripeColor1 : _StripeColor2;
+            
+            // Apply the stripe effect
+            o.Albedo = tex2D(_MainTex, IN.uv_MainTex).rgb * stripeColor.rgb;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
